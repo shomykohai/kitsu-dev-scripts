e7359ddb8a2fe8044ab3b89bad82b7b289b18649
Shomy <61943525+ShomyKohai@users.noreply.github.com>
2023-10-21 17:33:14 +0200

    Update Kitsu Builder
    * Replace urrliib with requests and add a progressbar
    * Fix Create User tool
    * Ask user to run the script as root (#1)
    * Add Seed tool (#2)
    * Fix Flipper flag enabler (#3)
    * Run db:setup before seeding database to resolve failed database migrations

--- kitsu_builder.py
+++ kitsu_builder.py
@@ -1,11 +1,13 @@
 import argparse
 import os
+import requests
 import sys
 import subprocess
 from colorama import Fore, Style
-from urllib import request, parse
+from getpass import getuser
 from ruamel import yaml
 from shutil import which
+from tqdm import tqdm
 from typing import (
     Optional
 )
@@ -31,6 +33,7 @@ def parse_args() -> None:
         help="Gain the PRIVILEGES! (Or: Set the given user permissions to those of super-admin); Provide the SLUG of your user with no quotes"
     )
     tools_parser.add_argument("--add-flag", '-ff', type=str, help="Enable a flag from Flipper.")
+    tools_parser.add_argument("--seed", '-s', action="store_true", help="Seed the database.")
     tools_parser.add_argument("--create-user", '-c', type=str, help="Create a user account with 'test' as password. Use only for if registrations don't work.")
 
 
@@ -58,6 +61,9 @@ def parse_args() -> None:
     if hasattr(args, 'create_user'):
         if args.create_user is not None:
             create_account(args.create_user, args.dev_path)
+    if hasattr(args, 'seed'):
+        if args.seed is True:
+            seed_database(args.dev_path)
 
 
 def check_valid_folder(dir: str) -> Optional[str]:
@@ -90,7 +96,7 @@ def gain_admin_powers(user: str, dev_env: str) -> None:
 
 def enable_flipper_flag(flag: str, dev_env: str) -> None:
     print(f"{Fore.YELLOW}Kitsu Builder {Fore.WHITE}> {Fore.GREEN}Enabling {flag} flag!{Style.RESET_ALL}")
-    rails_console = subprocess.Popen(['bin/rails', 'runner', f'\"Flipper[:{flag}].enable\"'], cwd=dev_env)
+    rails_console = subprocess.Popen(['bin/rails', 'runner', f'Flipper[:{flag}].enable'], cwd=dev_env)
     rails_console.wait()
 
 
@@ -101,14 +107,68 @@ def create_account(username: str, dev_env: str) -> None:
         "attributes": {
         "email": f"{username}@kitsu.dev",
         "name": username,
-        "password": "test"
+        "password": "test",
+        "slug": username
         },
         "type": "users"
         }
     }
-    data = parse.urlencode(payload).encode()
-    req = request.Request("http://kitsu.localhost:42069/api/edge/users", data=data, headers=headers)
+    req = requests.post("http://kitsu.localhost:42069/api/edge/users", json=payload, headers=headers)
+    print(f"{Fore.YELLOW}Kitsu Builder {Fore.WHITE}> {Fore.GREEN}Created user \"{username}\"!{Style.RESET_ALL}")
 
+
+def seed_database(dev_env: str) -> None:
+    KITSU_DB_DUMP = "https://f002.backblazeb2.com/file/kitsu-dumps/latest.sql.gz"
+    
+    # Since bin/seed download a .gz file, it should first extract the db dump, but
+    # For some reason the downloaded file is just a plain SQL file, so we must
+    # Import it using psql
+    KITSU_TOOLS_DIR = os.path.abspath(dev_env)
+    
+    # We first download the db dump
+    print(f"{Fore.YELLOW}Kitsu Builder {Fore.WHITE}> {Fore.CYAN}Downloading the DB dump, please wait for the download to complete and {Fore.RED}do not{Fore.CYAN} interrupt the process.{Style.RESET_ALL}")
+    # Code from https://stackoverflow.com/questions/56795227/how-do-i-make-progress-bar-while-downloading-file-in-python
+    with requests.get(KITSU_DB_DUMP, stream=True) as r:
+        with open(f"{KITSU_TOOLS_DIR}/anime.sql", 'wb') as f:
+            pbar = tqdm(total=int(r.headers['Content-Length']))
+            for chunk in r.iter_content(chunk_size=8192):
+                if chunk:
+                    f.write(chunk)
+                    pbar.update(len(chunk))
+    print(f"{Fore.YELLOW}Kitsu Builder {Fore.WHITE}> {Fore.GREEN}Download completed! Now we'll import it, please wait..{Style.RESET_ALL}\n")
+    
+    # And import it
+    # Instead of using the provided bin/seed, we will directly call docker compose exec 
+    # So we don't get "the input device is not a TTY" error message!
+    
+    # Before seeding we drop the schema manually
+    dockercommand = "docker compose exec -T -i postgres psql --username=kitsu_development --host=postgres -d kitsu_development --command"
+    dockercommand = dockercommand.split()
+    dockercommand.append("DROP SCHEMA public CASCADE;CREATE SCHEMA public;") # Append the query as an unique list element
+
+    docker_comp = subprocess.Popen(dockercommand, cwd=KITSU_TOOLS_DIR)
+    docker_comp.wait()
+
+    # Use a string since it's faster for me than writing all that stuff into a list 
+    # The -T parameter disable TTY, see https://docs.docker.com/engine/reference/commandline/compose_exec/
+    # Also we only seed the kitsu_development DB 
+    dockercommand = "docker compose exec -T -i postgres psql --username=kitsu_development --host=postgres kitsu_development"
+    
+    # Open the DB dump as a file and pass it to sdtin
+    # Set the cwd to the kitsu-tools one so we're sure that the postgres container is found
+    with open(f"{KITSU_TOOLS_DIR}/anime.sql", 'r') as f:
+        docker_comp = subprocess.Popen(dockercommand.split(), cwd=KITSU_TOOLS_DIR, stdin=f)
+        docker_comp.wait()
+
+    # And run the migrations
+    rake = subprocess.Popen([f'{KITSU_TOOLS_DIR}/bin/rake', 'db:migrate'])
+    rake.wait()
+    rake = subprocess.Popen([f'{KITSU_TOOLS_DIR}/bin/rake', 'chewy:reset']) # Reindex DB
+    rake.wait()
+
+    # After importing, we'll delete the DB dump since it's not used anymore and to free up space
+    os.remove(f"{KITSU_TOOLS_DIR}/anime.sql")
+    print(f"{Fore.YELLOW}Kitsu Builder {Fore.WHITE}> {Fore.GREEN}Database imported (anime.sql > kitsu_development)!{Style.RESET_ALL}\n")
 
 def setup(path: str, should_seed: bool = False) -> None:
     cwd = path
@@ -153,12 +213,12 @@ def setup(path: str, should_seed: bool = False) -> None:
     with open(f"{cwd}/kitsu-tools/docker-compose.yml", 'w') as f:
         yamlparser.dump(contents, f)
     
-    # # Clone the server
+    # Clone the server
     print(f"{Fore.YELLOW}Kitsu Builder {Fore.WHITE}> {Fore.GREEN}Cloning server.{Style.RESET_ALL}\n")
     gitclone = subprocess.Popen(['git', 'clone', "https://github.com/hummingbird-me/kitsu-server.git", f"{cwd}/kitsu-tools/server"])
     gitclone.wait()
 
-    # # Then the client with already applied changes
+    # Then the client with already applied changes
     print(f"{Fore.YELLOW}Kitsu Builder {Fore.WHITE}> {Fore.GREEN}Cloning client (from {Fore.CYAN}ShomyKohai/kitsu-web@the-future{Fore.GREEN}).{Style.RESET_ALL}\n")
     gitclone = subprocess.Popen(['git', 'clone', '-b', 'the-future', "https://github.com/ShomyKohai/kitsu-web.git", f"{cwd}/kitsu-tools/client"])
     gitclone.wait()
@@ -178,62 +238,25 @@ def setup(path: str, should_seed: bool = False) -> None:
     docker_comp = subprocess.Popen([f'{cwd}/kitsu-tools/bin/start'])
     docker_comp.wait()
 
+    # HACK: We run db:setup on rails to be sure that we don't encounter an issue when running migrations
+    rails_console = subprocess.Popen(["bin/rails", "db:setup"], cwd=cwd)  
+
     # Now we seed the database if the user chose to
-    if should_seed:
-        KITSU_DB_DUMP = "https://f002.backblazeb2.com/file/kitsu-dumps/latest.sql.gz"
-        
-        # Since bin/seed download a .gz file, it should first extract the db dump, but
-        # For some reason the downloaded file is just a plain SQL file, so we must
-        # Import it using psql
-        KITSU_TOOLS_DIR = f"{cwd}/kitsu-tools"
-        
-        # We first download the db dump
-        print(f"{Fore.YELLOW}Kitsu Builder {Fore.WHITE}> {Fore.CYAN}Downloading the DB dump, please wait for the download to complete and {Fore.RED}do not{Fore.CYAN} interrupt the process.{Style.RESET_ALL}")
-        request.urlretrieve(KITSU_DB_DUMP, f"{KITSU_TOOLS_DIR}/anime.sql")
-        print(f"{Fore.YELLOW}Kitsu Builder {Fore.WHITE}> {Fore.GREEN}Download completed! Now we'll import it, please wait..{Style.RESET_ALL}\n")
-        
-        # And import it
-        # Instead of using the provided bin/seed, we will directly call docker compose exec 
-        # So we don't get "the input device is not a TTY" error message!
-        
-        # Before seeding we drop the schema manually
-        dockercommand = "docker compose exec -T -i postgres psql --username=kitsu_development --host=postgres -d kitsu_development --command"
-        dockercommand = dockercommand.split()
-        dockercommand.append("DROP SCHEMA public CASCADE;CREATE SCHEMA public;") # Append the query as an unique list element
-
-        docker_comp = subprocess.Popen(dockercommand, cwd=KITSU_TOOLS_DIR)
-        docker_comp.wait()
-
-        # Use a string since it's faster for me than writing all that stuff into a list 
-        # The -T parameter disable TTY, see https://docs.docker.com/engine/reference/commandline/compose_exec/
-        # Also we only seed the kitsu_development DB 
-        dockercommand = "docker compose exec -T -i postgres psql --username=kitsu_development --host=postgres kitsu_development"
-        
-        # Open the DB dump as a file and pass it to sdtin
-        # Set the cwd to the kitsu-tools one so we're sure that the postgres container is found
-        with open(f"{KITSU_TOOLS_DIR}/anime.sql", 'r') as f:
-            docker_comp = subprocess.Popen(dockercommand.split(), cwd=KITSU_TOOLS_DIR, stdin=f)
-            docker_comp.wait()
-
-        # And run the migrations
-        rake = subprocess.Popen([f'{KITSU_TOOLS_DIR}/bin/rake', 'db:migrate'])
-        rake.wait()
-        rake = subprocess.Popen([f'{KITSU_TOOLS_DIR}/bin/rake', 'chewy:reset']) # Reindex DB
-        rake.wait()
-
-        # After importing, we'll delete the DB dump since it's not used anymore and to free up space
-        os.remove(f"{KITSU_TOOLS_DIR}/anime.sql")
-        print(f"{Fore.YELLOW}Kitsu Builder {Fore.WHITE}> {Fore.GREEN}Database imported (anime.sql > kitsu_development)!{Style.RESET_ALL}\n")
+    if should_seed: seed_database(cwd)
 
     # Finally we have the last steps
 
     # First we enable registrations in the server so it's possible to create an account from the web page
     print(f"{Fore.YELLOW}Kitsu Builder {Fore.WHITE}> {Fore.GREEN}Now we enable registrations in the server!{Style.RESET_ALL}")
-    rails_console = subprocess.Popen([f'{cwd}/kitsu-tools/bin/rails', 'runner', '\"Flipper[:registration].enable\"'])
-    rails_console.wait()
+    enable_flipper_flag("registration", f"{cwd}/kitsu-tools")
 
     print(f"{Fore.YELLOW}Kitsu Builder {Fore.WHITE}> {Fore.GREEN}Setup completed!{Style.RESET_ALL}")
 
 
 if __name__ == '__main__':
-    parse_args()
+    # Check if run with root because of some random errors that may occur when dealing with docker
+    if getuser() == "root":
+        parse_args()
+    else:
+        print(f"{Fore.YELLOW}Kitsu Builder {Fore.WHITE}> {Fore.RED}Please run the script as root.{Style.RESET_ALL}")
+
